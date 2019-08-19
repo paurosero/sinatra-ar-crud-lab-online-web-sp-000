a757577ca953733098f10991ca7bed40c24011f4
Paul Rosero <paul_rosero@yahoo.com>
2019-08-19 23:17:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,7 +47,7 @@ class ApplicationController < Sinatra::Base
   delete '/articles/:id' do
     @article = Article.find_by(id: params[:id])
     @article.destroy
-    
+    binding.pry
     redirect "/articles/index"
   end
   
